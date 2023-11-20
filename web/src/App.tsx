9c249de4e087a3d7347171ce8beed024749e6a6f
iivkis <razmolodinivan@mail.ru>
2023-11-20 19:55:47 +0700

    init web

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
